e58e922a168a4050ed0e0d5390ab8e43cab09f77
chairxiao <1277163268@qq.com>
2020-01-10 19:31:57 +0800

    edit html

--- plugin.rb
+++ plugin.rb
@@ -1,29 +1,37 @@
 # frozen_string_literal: true
-# name: xinhuanet-discourse-onebox
+# name: discourse-onebox-xinhuanet
 # about: For generating pretty oneboxes for xinhuanet.com
 # version: 0.1
 # authors: dengzi
 
 Onebox = Onebox
-module Onebox
-    module Engine
-      class XinhuaNetOnebox
-        include Engine
-        include LayoutSupport
-        include HTML
-  
-        matches_regexp(/^(http?:\/\/)?(www\.)?xinhuanet\.com\/(.)+\/?$/)
 
-        private
-  
-        def data
-          {
-            url: @url,
-            title: raw.css('title').text.sub(/-新华网/, '').strip(),
-            image: raw.css('.net-logo img').first['src'],
-            description: raw.css('meta[name=description]').first['content']
-          }
-        end
+module Onebox
+  module Engine
+    class XinhuaNetOnebox
+      include Engine
+
+      matches_regexp(/^(http?:\/\/)?(www\.)?xinhuanet\.com\/(.)+\/?$/)
+
+      def to_html
+        result ={
+          url: @url,
+          title: raw.css('title').text.sub(/-新华网/, '').strip(),
+          image: raw.css('.net-logo img').first['src'],
+          description: raw.css('meta[name=description]').first['content']
+        }
+
+        <<-HTML
+        <div class="onebox">
+          <a href="#{@url}">
+            <h3>#{result[:title]}</h3>
+            <img src="#{result[:image]}" />
+            <p>#{result[:description]}</p>
+          </a>
+      </div>
+      HTML
+      
       end
     end
   end
+end
